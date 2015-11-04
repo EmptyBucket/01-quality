0021cf8e8bd340ae2420cd8fc95ccfb5f947f056
Политов Алексей Николаевич <zxfess@yandex.ru>
2015-11-05 04:41:17 +0500

    сменил стримы на File

--- ConverterMarkdown/Program.cs
+++ ConverterMarkdown/Program.cs
@@ -14,10 +14,7 @@ namespace ConverterMarkdown
             if (typeInFile != ".md")
                 throw new FileFormatException("Expected type .md, received {typeInFile}");
 
-            string rawFileStr;
-            using (var fileStream = new FileStream(pathInMarkdown, FileMode.Open))
-            using (var streamReader = new StreamReader(fileStream))
-                rawFileStr = streamReader.ReadToEnd();
+            string rawFileStr = File.ReadAllText(pathInMarkdown);
 
             MarkdownObjectToHTMLParser toHTMLParser = new MarkdownObjectToHTMLParser();
             MarkdownToTypeConverter converterMarkdown = new MarkdownToTypeConverter(toHTMLParser);
@@ -32,9 +29,7 @@ namespace ConverterMarkdown
             {
                 pathOut = Path.Combine(Environment.CurrentDirectory, "OutHTML.html");
             }
-            using (var fileStream = new FileStream(pathOut, FileMode.Create))
-            using (var streamWriter = new StreamWriter(fileStream))
-                streamWriter.Write(html);
+            File.WriteAllText(pathOut, html);
         }
     }
 }
