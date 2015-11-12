b76026ca4c771686d956e6c93d83adcfe1e1dc4d
Политов Алексей Николаевич <zxfess@yandex.ru>
2015-11-13 03:06:21 +0500

    обработка аргументов командной строки

--- ConverterMarkdown/Program.cs
+++ ConverterMarkdown/Program.cs
@@ -9,10 +9,18 @@ namespace ConverterMarkdown
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Type 1 command-line argument - the path to the input file markdaun 2 command-line argument (optional) - the path to the output file html");
+                return;
+            }
             string pathInMarkdown = args[0];
             string typeInFile = Path.GetExtension(pathInMarkdown);
             if (typeInFile != ".md")
-                throw new FileFormatException("Expected type .md, received {typeInFile}");
+            {
+                Console.WriteLine($"Expected type in file .md, received {typeInFile}");
+                return;
+            }
 
             string rawFileStr = File.ReadAllText(pathInMarkdown);
 
@@ -21,13 +29,17 @@ namespace ConverterMarkdown
             string html = converterMarkdown.Convert(rawFileStr);
 
             string pathOut;
-            try
+            if(args.Length < 2)
+                pathOut = Path.Combine(Environment.CurrentDirectory, "OutHTML.html");
+            else
             {
                 pathOut = args[1];
-            }
-            catch (IndexOutOfRangeException)
-            {
-                pathOut = Path.Combine(Environment.CurrentDirectory, "OutHTML.html");
+                string typeOutFile = Path.GetExtension(pathOut);
+                if (typeOutFile != ".html")
+                {
+                    Console.WriteLine($"Expected type out file html, received {typeOutFile}");
+                    return;
+                }
             }
             File.WriteAllText(pathOut, html);
         }
